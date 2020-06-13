77d814fd4dd111cafd5b2025c720fca91f5b1af7
Silvia <sarnaizgarcia@gmail.com>
2020-06-13 12:35:25 +0200

    Base and index template

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/index')
+def index():
+    user = {'username': 'Silvia'}
+    files = [
+        {
+            'author': {'username': 'Silvia'},
+            'file_name': 'file1'
+        },
+        {
+            'author': {'username': 'Roberto'},
+            'file_name': 'file2'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, files=files)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001)
