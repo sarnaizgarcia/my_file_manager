0c11d838408da42e411da04bc987afd1ebb877df
Silvia <sarnaizgarcia@gmail.com>
2020-06-14 17:18:00 +0200

    File model created

--- models.py
+++ models.py
@@ -1,11 +1,26 @@
+from datetime import datetime
+from random import randint
+
 from main import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(60), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    can_delete = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(60), index=True, unique=True)
+    upload_date = db.Column(db.Datetime, index=True, default=datetime.utcnow)
+    size = db.Column(db.Integer, default=randint(200, 900))
+    hash_sha = db.Column(db.Integer, default=randint(200, 900))
+
+    def __repr__(self):
+        return f'<File {self.file_name}>'
