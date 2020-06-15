5964aa7f393a48565e8d36970eeb8c64d513633f
Silvia <sarnaizgarcia@gmail.com>
2020-06-15 18:09:09 +0200

    Created methods in user models to generate hash and check password

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from random import randint
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 
 
@@ -11,6 +13,12 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     can_delete = db.Column(db.Boolean, default=False)
     files = db.relationship('File', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.username}>'
